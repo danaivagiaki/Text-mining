dd36c21e546f9cd74b3a3007cbc08b076d54b70b
danaivagiaki <danaivag@outlook.com>
2020-08-07 19:56:18 +0200

    Update generate_output_w_text.sh

--- generate_output_w_text.sh
+++ generate_output_w_text.sh
@@ -13,7 +13,6 @@ do
     echo -e "$pid\t$match"
     # PMID, match, title, text
     gzip -cd ../abstracts/*.tsv.gz | grep -E "\<$pid\>" | sort -u | cut -f 5,6 | sed 's/\s\s\+//g' | tr -d '\n' | awk -v RS="\t" -v ORS="\t" -v FS="" -v OFS="" '(NR==1){print $0; prev_line=$0};(NR==2){print prev_line$0;}' | awk -v RS="\t" -v ORS="" -v FS="" -v OFS="" -v id="$pid" -v s="$f" -v e="$l" -v m="$match" -v w=30 -f /home/danvag/Lifestyle_Factors/tmp2_coordinates2text.awk >> lf_output_with_text.tsv
-#    sed -i ':a;$!{N;s/\n\n/\n/;ba;}' tmp4_lf_output_with_text.tsv
 done < "$coords" # "${1:-/dev/stdin}"
 
 # sed ':a;$!{N;s/\n\n/\n/;ba;}'
