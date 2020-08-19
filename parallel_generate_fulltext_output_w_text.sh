58eb6317c37ee450b4acca35962a80e8fc7e3fdd
danaivagiaki <danaivag@outlook.com>
2020-08-19 15:22:46 +0200

    Update parallel_generate_fulltext_output_w_text.sh

--- parallel_generate_fulltext_output_w_text.sh
+++ parallel_generate_fulltext_output_w_text.sh
@@ -7,5 +7,5 @@ do
     l=$(echo $line | awk '{print $3}')
     match=$(echo $line | awk -v FS=" " -v ORS=" " '{for (i=4;i<=NF;i++) print $i}')
     echo -e "$pid\t$match"
-    gzip -cd ./PMC*.tsv.gz | grep -E "\<$pid\>" | sort -u | tr -d '\n' | awk -v FS="\t" -v ORS="\t" '{for(i=5;i<=NF;i++){print $i}}' | awk -v RS="\t" -v ORS="" -v FS="" -v OFS="" '(NR==1){print $0, "\t"; title=$0}; {if(NR>=2){print title; for(i=2;i<=NR;i++){print $0}}};' | awk -v RS="\t" -v ORS="" -v FS="" -v OFS="" -v id="$pid" -v s="$f" -v e="$l" -v m="$match" -v w=30 -f ./coordinates2txt.awk >> Lifestyle_Factor_Ontology_wo_Obsolete_fulltext_output_for_review_with_text.tsv
+    gzip -cd ./PMC*.tsv.gz | grep -E "\<$pid\>" | sort -u | tr -d '\n' | awk -v FS="\t" -v ORS="\t" '{for(i=5;i<=NF;i++){print $i}}' | awk -v RS="\t" -v ORS="" -v FS="" -v OFS="" '(NR==1){print $0, "\t", $0}; {if(NR>=2){print $0}};' | awk -v RS="\t" -v ORS="" -v FS="" -v OFS="" -v id="$pid" -v s="$f" -v e="$l" -v m="$match" -v w=30 -f ./coordinates2text.awk >> Lifestyle_Factor_Ontology_wo_Obsolete_fulltext_output_for_review_with_text.tsv
 done < "$1"
